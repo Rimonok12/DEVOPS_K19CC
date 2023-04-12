6526fd1bec40c0dffc046b41893d352e4a176d3a
RIMON DEBNATH <68506201+Rimonok12@users.noreply.github.com>
2023-04-12 11:31:02 +0530

    updated

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  access_key = "AKIASF6DGM6UES2MWDOS"
-  secret_key = "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"
   region     = "ap-south-1"
 }
 resource "aws_s3_bucket" "new_bucket" {
