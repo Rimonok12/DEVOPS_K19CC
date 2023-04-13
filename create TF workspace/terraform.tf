d7e1ff6100b7ca2b2b7b9309a398ccbb8ef03537
RIMON DEBNATH <68506201+Rimonok12@users.noreply.github.com>
2023-04-13 12:30:16 +0530

    Create a Workspace
    Create a Workspace in Terraform .

---
+++ create TF workspace/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "rimon-TF"
+
+    workspaces {
+      name = "learn-terraform-cloud-2"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
